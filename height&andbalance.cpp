b2dc2d80467381a5e2293926ccd43db8f6f5a70c
Ariri-Boniface <bonifacearirim@gmail.com>
2025-09-21 12:19:52 +0300

    Add files via upload

---
+++ height&andbalance.cpp
@@ -0,0 +1,22 @@
+/*
+Name:Ariri Boniface
+Reg No:PA106/G/28780/25
+Description: program for User prompts
+*/
+#include <stdio.h>
+int main (){
+ int height; // %d
+ float id; // %f
+ double balance;  // %fl
+ 
+ printf("\nEnter your height: ")
+ scanf("%d", &height)
+ 
+ printf("\nEnter your id: ")
+ scanf("%f", &id)
+ 
+ printf("\nEnter your bank balance: ")
+ scanf("%fl", &balance)
+ 
+ return 0;
+}
