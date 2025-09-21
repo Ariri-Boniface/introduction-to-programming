2dcbfb989b916eccab1241c7c979643e3936f196
Ariri-Boniface <bonifacearirim@gmail.com>
2025-09-21 12:13:18 +0300

    Add files via upload

---
+++ unitsforsemONE.c
@@ -0,0 +1,24 @@
+/*
+Name:ARIRI BONIFACE
+Reg No:PA106/G/28780/25
+Description:UNITS FOR 1ST SEMESTER
+*/
+#include <stdio.h>
+int main (){
+	
+	printf(" \nsystem analysis and design");
+	
+	printf("\nfundermentals of ict");
+	
+	printf("\nComputer systems and organization");
+	
+	printf("\nDiscrete mathematics");
+	
+	printf("\nIntroduction to programming ");
+	
+    printf("\nCommunication skills ");
+    
+ 	printf("\nMathematics for science ");
+	
+	return 0;
+}
