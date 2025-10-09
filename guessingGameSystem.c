ffd005f7c67927f29597d91d400a7a53cb69edde
Ariri-Boniface <bonifacearirim@gmail.com>
2025-10-09 15:51:02 +0300

    Add files via upload

---
+++ guessingGameSystem.c
@@ -0,0 +1,29 @@
+/*
+Name:
+Reg No:
+Description:
+*/
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int secretNumber, guess, attempts = 0;
+    srand(time(0));
+    secretNumber = (rand() % 20) + 1;
+     printf("Guess the number (between 1 and 20):\n");
+	do {
+	        printf("Enter guess: ");
+	        scanf("%d", &guess);
+	        attempts++;
+	
+	        if (guess > secretNumber) {
+	            printf("Too high!\n");
+	        } else if (guess < secretNumber) {
+	            printf("Too low!\n");
+	        } else {
+	            printf("You guessed it in %d attempts.\n", attempts);
+	        }
+	    } while (guess != secretNumber);
+	
+	    return 0;
+	}
