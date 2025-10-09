1b373c813be89e6f3bcc616ab00dddd5e8c9696a
Ariri-Boniface <bonifacearirim@gmail.com>
2025-10-09 15:58:26 +0300

    Add files via upload

---
+++ passwordSystem.c
@@ -0,0 +1,22 @@
+/*Name: ARIRI BONIFACE
+Reg No: PA106/G/28780/25
+Description: password system
+*/
+#include <stdio.h>
+
+int main() {
+    int password;
+    do {
+        printf("Enter password: ");
+        scanf("%d", &password);
+
+        if (password != 1234) {
+            printf("Incorrect password. Try again.\n");
+        }
+
+    } while (password != 1234);
+
+    printf("Access Granted.\n");
+
+    return 0;
+}
